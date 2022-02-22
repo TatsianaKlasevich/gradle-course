c0e36b455dbb1b5355916b7fc243a351a2c05308
Tatsiana Klasevich <klasevich.t@gmail.com>
2022-02-22 15:44:34 +0300

    change modules

--- src/main/java/com/klasevich/service/UserService.java
+++ src/main/java/com/klasevich/service/UserService.java
@@ -6,7 +6,9 @@ import com.klasevich.model.User;
 import java.util.List;
 
 public class UserService {
+
     private final UserDao userDao = new UserDao();
+
     public List<User> getAll(){
         return userDao.findAll();
     }

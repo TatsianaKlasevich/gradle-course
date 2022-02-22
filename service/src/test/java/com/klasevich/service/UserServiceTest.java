eeb3b52e498d8c38b5d494f21beb4a8ea543abc3
Tatsiana Klasevich <tatsiana.klasevich@itrexgroup.com>
2022-02-22 19:20:50 +0300

    add jacoco plugin

---
+++ service/src/test/java/com/klasevich/service/UserServiceTest.java
@@ -0,0 +1,20 @@
+package com.klasevich.service;
+
+import com.klasevich.model.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    private final UserService userService = new UserService();
+
+    @Test
+    void getAll() {
+        List<User> actual = userService.getAll();
+        assertEquals(3, actual.size());
+
+    }
+}
